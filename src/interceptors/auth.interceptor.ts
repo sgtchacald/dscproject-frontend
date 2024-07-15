f6bf2aef600fbb3348a035f60a5f12f831555fc6
Diego Cordeiro <sgt.chacal.d@gmail.com>
2024-07-15 20:42:24 -0300

    Feat: Framework adicionando interceptor.

---
+++ src/interceptors/auth.interceptor.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+
+  constructor() {}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    console.log('intercept request', request);
+    const token = localStorage.getItem('token'); // Recuperar token do armazenamento local
+
+    if(token) {
+      request = request.clone({
+        //headers: request.headers.set('Authorization', `Bearer ${token}`)
+      });
+    }
+
+    return next.handle(request);
+  }
+}
